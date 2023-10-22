e7e343acd13ca569cf5ffa3b7f348f5b1bc11f6b
Cornejo Sosa <615086@bah.com>
2023-10-22 11:12:45 -0500

    added required variables

--- variables.tf
+++ variables.tf
@@ -4,5 +4,24 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-2"
+  default     = "us-west-2"
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  
+}
+
+variable "aws_access_key" {
+  description = "AWS secret key"
+  type        = string
+  
+}
+
+variable "organization" {
+  description = "Organization name"
+  type        = string
+  default     = "Phalanx_Spear"
+  
 }
